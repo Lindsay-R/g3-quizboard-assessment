9261e6782c5557bca1656ae0a109b39bfd5a17a2
Lindsay-R <lrecknell@gmail.com>
2014-10-20 10:39:09 -0600

    created buttons and started JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,59 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+$(document).ready(function() {
+
+
+
+
+  $(document).on("ready", function () {
+    var promiseOfResult = $.getJSON("/quizzes");
+
+    var format_answer_item = function (answer) {
+      var answer_item_var =
+        "<li>" + answer["answer_item"] + "</li>";
+
+      return answer_item_var;
+
+    };
+
+    var whatToDoWhenItSucceeds = function (jsonResponse) {
+      var todo_items_var = jsonResponse.map(format_answer_item );
+
+      $('.js-output ul').html(todo_items_var);
+    };
+
+    promiseOfResult.success(whatToDoWhenItSucceeds);
+
+  });
+
+
+
+
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    var answer =$('#first-answer').val();
+
+
+    $.ajax({
+      type: "POST",
+      url: "/quizzes",
+      data: {
+       answer_item: answer
+      }
+    });
+
+
+
+    console.log(answer);
+
+    $('#answer-form').append("<div class='pop'>"+ answer +'</div>');
+
+
+
+  });
+
+
+});
--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,10 +2,34 @@ class QuizzesController < ApplicationController
 
   def index
     @quizzes = Quiz.all
+
+
+    @answer =Quiz.new
+
+    @answers = Quiz.order(:answer_item)
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @answers }
+    end
+
+
   end
 
   def show
     @quiz = Quiz.find(params[:id])
+  end
+
+
+  def create
+
+    @answer = Quiz.new(
+      :answer_item => params[:answer_item]
+    )
+    if @answer.save
+      render json: @answer
+    end
+
   end
 
 
